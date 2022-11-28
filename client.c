cb1e29f3663cad514495826393887f0531ce6289
Identityless <rlandwnd555@naver.com>
2022-11-28 20:08:23 +0900

    feat : 사용 가능한 SharedMemory 스캔 기능 구현

--- client.c
+++ client.c
@@ -17,6 +17,7 @@
 #define SHMKEY9 (key_t)60119;
 #define MEMSIZE 4;
 
+void checkUsableKey();
 
 void main() {
     int msg;
@@ -25,8 +26,7 @@ void main() {
     key_t key = SHMKEY0;
     void *memory_segment=NULL;
 
-    printf("데이터 입력 : ");
-    scanf("%d", &msg);
+    checkUsableKey();
 
     if((shmid=shmget(key,sizeof(int),IPC_CREAT|0666))==-1) {
         printf("shmget faild\n");
@@ -37,10 +37,30 @@ void main() {
         exit(0);
     }
 
+}
 
-    if((memory_segment=shmat(shmid,NULL,0))==(void*)-1){
-            printf("shmat failed\n");
+void checkUsableKey() {
+    key_t keys[] = {(key_t)60110, (key_t)60111, (key_t)60112, (key_t)60113, (key_t)60114, (key_t)60115, (key_t)60116, (key_t)60117, (key_t)60118, (key_t)60119};
+    int shmid[10];
+    void *memory_segment=NULL;
+    int readpoint;
+
+    for(int i = 0 ; i < 10 ; i++) {
+        if((shmid[i]=shmget(keys[i],sizeof(int),IPC_CREAT|0666))==-1) {
+            printf("shmget faild. (point 1)\n");
+        }
+        if((memory_segment=shmat(shmid[i],NULL,0))==(void*)-1) {    // shared moemory 포인터를 바꿔가면서 검사
+            printf("shmat failed. (point 2)\n");
             exit(0);
-    }
+        }
+        memcpy(&readpoint, (int*)memory_segment, sizeof(int));
 
+        if(readpoint == -1)
+        {
+            printf("%d key - 사용 가능\n", (int)keys[i]);
+        }
+        else {
+            printf("%d key - 사용 불가\n", (int)keys[i]);
+        }
+    }
 }
